b6bd0552cc9bf0196aede9a7cb12b6c280916f2d
jfspps <jfsapps@gmail.com>
2021-01-13 22:53:54 +0000

    project setup

---
+++ web_frontend/src/main/java/com/algo_web/demo/AlgoInJavaApp.java
@@ -0,0 +1,13 @@
+package com.algo_web.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoInJavaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoInJavaApp.class, args);
+    }
+
+}
---
+++ web_frontend/src/test/java/com/algo_web/demo/AlgoInJavaAppTests.java
@@ -0,0 +1,13 @@
+package com.algo_web.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoInJavaAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
