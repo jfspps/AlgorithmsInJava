1f22ba341ae07b4f3c8e51d4d4c1ddd32834660d
jfspps <jfsapps@gmail.com>
2021-01-20 11:15:01 +0000

    added LinkedLists controller and index template

---
+++ web_frontend/src/main/java/com/algo_web/demo/controllers/LinkedListsController.java
@@ -0,0 +1,17 @@
+package com.algo_web.demo.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/linkedlists")
+public class LinkedListsController {
+
+    @GetMapping({"/", ""})
+    public String getIndex(){
+        return "/linkedlists/index";
+    }
+}
