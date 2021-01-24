9a2431f2f1c221b30c8f0c7a80f170e89d9fa8e6
jfspps <jfsapps@gmail.com>
2021-01-24 21:36:47 +0000

    added custom Stack and Queue classes, with unit tests

---
+++ ADTs_algo/src/test/java/com/algo_data/stacksAndQueues/QueueTest.java
@@ -0,0 +1,48 @@
+package com.algo_data.stacksAndQueues;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueueTest {
+
+    Queue<String> stringQueue;
+    String first;
+    String second;
+
+    @BeforeEach
+    void setUp() {
+        first = "first";
+        second = "second";
+        stringQueue = new Queue<>();
+        stringQueue.enqueue(first);
+        stringQueue.enqueue(second);
+    }
+
+    @Test
+    void enqueueAndDequeue() {
+        assertEquals(first, stringQueue.dequeue());
+        assertEquals(second, stringQueue.dequeue());
+        assertThrows(NoSuchElementException.class, () -> {
+            stringQueue.dequeue();
+        });
+    }
+
+    @Test
+    void peek() {
+        assertEquals(first, stringQueue.peek());
+        assertNotEquals(second, stringQueue.dequeue());
+    }
+
+    @Test
+    void isEmpty() {
+        assertFalse(stringQueue.isEmpty());
+        stringQueue.dequeue();
+        assertFalse(stringQueue.isEmpty());
+        stringQueue.dequeue();
+        assertTrue(stringQueue.isEmpty());
+    }
+}
