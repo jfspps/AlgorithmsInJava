7f532ef0d69a9fd212198a8c488451829370a67a
jfspps <jfsapps@gmail.com>
2021-01-24 21:50:04 +0000

    added array-based implementation of Stack, ArrayStack, with tests

---
+++ ADTs_algo/src/main/java/com/algo_data/stacksAndQueues/ArrayStack.java
@@ -0,0 +1,53 @@
+package com.algo_data.stacksAndQueues;
+
+import java.util.EmptyStackException;
+
+// array implementation of a stack
+public class ArrayStack<T> {
+
+    // Java would use boxed types (Integer, String, Character)
+    // approach by storing items in an array, where the top item/node is updated after each method
+
+    private final int stackSize;
+    private final T[] stackArray;
+    private int topIndex;
+
+    public ArrayStack(int stackSize){
+        //care needed to keep this execution away from client code until T is known
+        this.stackSize = stackSize;
+
+        //remember this for ArrayList et. al. ADT implementations!
+        this.stackArray = (T[]) new Object[this.stackSize];
+        this.topIndex = -1;
+    }
+
+    public T push(T data) {
+        // check if stack is already full (unlike linked lists)
+        if (this.topIndex == this.stackSize - 1){
+            System.out.println("Stack is full, cannot push to stack");
+            return null;
+        }
+        this.stackArray[++this.topIndex] = data;
+        return data;
+    }
+
+    public T pop(){
+        if (topIndex == -1){
+            System.out.println("Stack is empty");
+            throw new EmptyStackException();
+        }
+        return stackArray[topIndex--];
+    }
+
+    public T peek(){
+        if (topIndex == -1){
+            System.out.println("Stack is already empty");
+            return null;
+        }
+        return stackArray[topIndex];
+    }
+
+    public boolean isEmpty(){
+        return topIndex < 0;
+    }
+}
---
+++ ADTs_algo/src/test/java/com/algo_data/stacksAndQueues/ArrayStackTest.java
@@ -0,0 +1,54 @@
+package com.algo_data.stacksAndQueues;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayStackTest {
+
+    ArrayStack<String> stringArrayStack;
+    String first;
+    String second;
+
+    @BeforeEach
+    void setUp() {
+        stringArrayStack = new ArrayStack<>(2);
+        first = "first";
+        second = "second";
+        stringArrayStack.push(first);
+        stringArrayStack.push(second);
+    }
+
+    @Test
+    void pop() {
+        assertEquals(second, stringArrayStack.pop());
+        assertEquals(first, stringArrayStack.pop());
+        assertThrows(EmptyStackException.class, () -> {
+            stringArrayStack.pop();
+        });
+    }
+
+    @Test
+    void push() {
+        String temp = "tempElement";
+        assertNull(stringArrayStack.push(temp));
+    }
+
+    @Test
+    void peek() {
+        assertEquals(second, stringArrayStack.peek());
+        assertNotEquals(first, stringArrayStack.peek());
+    }
+
+    @Test
+    void isEmpty() {
+        assertFalse(stringArrayStack.isEmpty());
+        stringArrayStack.pop();
+        assertFalse(stringArrayStack.isEmpty());
+        stringArrayStack.pop();
+        assertTrue(stringArrayStack.isEmpty());
+    }
+}
